67dbbd628b3024d7bead1f0d58cd5856117d0ecb
andregarcia0412 <andregarcia0412@gmail.com>
2026-08-06 00:09:22 -0300

    :sparkles: feature: extract functions to data.py

---
+++ data.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    df_drivers = pd.read_csv("./data/drivers.csv")
+    df_drivers["fullName"] = df_drivers["forename"] + " " + df_drivers["surname"] 
+    return (
+        df_drivers,
+        pd.read_csv("./data/races.csv"),
+        pd.read_csv("./data/driver_standings.csv"),
+        pd.read_csv("./data/results.csv"),
+    )
+
+def wins_by_gp(df_results: pd.DataFrame, df_races: pd.DataFrame, driver_id: int):
+    df_driver_results = df_results[df_results["driverId"] == driver_id]
+    won_race_ids = df_driver_results[df_driver_results["positionOrder"] == 1]["raceId"].to_numpy()
+    won_gp_counts = df_races[df_races["raceId"].isin(won_race_ids)]["name"].value_counts()
+    
+    return (won_gp_counts, len(df_driver_results))
+
+def average_position(driver_id: int, df_results: pd.DataFrame):
+    return pd.to_numeric(df_results[df_results["driverId"] == driver_id]["position"], errors="coerce").mean()
